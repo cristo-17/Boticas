2ebcfcafa8f7dcede8c726506c42507f364e8ce7
Cristopher Cochachi Flores <cristofercochachiflores@gmail.com>
2026-09-08 21:01:39 -0500

    Arranque

--- backend/src/main/java/com/botica/backend/BackendApplication.java
+++ backend/src/main/java/com/botica/backend/BackendApplication.java
@@ -1,12 +1,11 @@
 package com.botica.backend;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class BackendApplication {
 	public static void main(String[] args) {
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		// Cambia "mi_contraseña_secreta" por la contraseña real que quieres usar
-		String miHash = encoder.encode("mi_contraseña_secreta");
-		System.out.println("Tu hash es: " + miHash);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 }
